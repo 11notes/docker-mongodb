622ff28744a081c08543d0d3ef80dbe945745dc7
ElevenNotes <github@11notes.ch>
2026-02-23 13:30:50 +0100

    cut: unix socket

--- rootfs/usr/local/bin/entrypoint.sh
+++ rootfs/usr/local/bin/entrypoint.sh
@@ -3,8 +3,11 @@
     set -- /usr/bin/mongod \
       --bind_ip 0.0.0.0 \
       --port 27017 \
+      --noauth \
       --dbpath /mongodb/var \
-      --unixSocketPrefix /mongodb/run
+      --nounixsocket \
+      --quiet \
+      --redactClientLogData
     eleven log start
   fi
 
